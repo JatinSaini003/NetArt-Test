3ec31f274d1353bd7d8314364c87f6e452d83880
Jatin Saini <jatinkhushi5555@gmail.com>
2024-02-27 22:04:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import logo from '../src/asserts/logo.png';
+import award from '../src/asserts/1.png';
+import intro_image from '../src/asserts/2.png';
+import product_Image from '../src/asserts/3.png';
+import { FaFacebookF } from "react-icons/fa6";
+import { CiGlobe } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
+import data from './data.js';
 import './App.css';
 
 function App() {
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="header_logo">
+        <img src={logo} alt='CRI_Fluid_Systems Logo'></img>
+      </div>
+
+      <div className="intro_section">
+        <div className='award'>
+          <img src={award} alt="" />
+        </div>
+
+        <div className="introduction">
+          <div className="intro_head">
+            <h4>{data.intro_head_line}</h4>
+            <ul>
+              <li>{data.intro_head_p1}</li>
+              <li>{data.intro_head_p2}</li>
+            </ul>
+          </div>
+          <img src={intro_image} alt="Award_receiving_Image"></img>
+          <div className="intro_tail">
+            <p>{data.intro_tail}</p>
+          </div>
+        </div>
+      </div>
+
+      <p className='middle_section'>{data.mid_section_text}</p>
+
+      <div className="product_image">
+        <img src={product_Image} alt="Products_Image" />
+        <figcaption>{data.image_caption}</figcaption>
+      </div>
+
+      <div className="bottom_section">
+        <h5>{data.footer_head}</h5>
+        <p>{data.footer_text}</p>
+      </div>
+
+      <footer>
+        <div className="contactdiv footer">
+          <div className="Icon">
+            <FaPhoneAlt />
+          </div>
+          <p>{data.contact}</p>
+        </div>
+        <div className="facebookdiv footer">
+          <div className="Icon">
+            <FaFacebookF />
+          </div>
+          <p>{data.facebook}</p>
+        </div>
+        <div className="websitediv footer">
+          <div className="Icon globe">
+            <CiGlobe size={50} />
+          </div>
+          <p>{data.website}</p>
+        </div>
+      </footer>
+
     </div>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,16 @@
+
+const data = {
+    "intro_head_line": "C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.",
+    "intro_head_p1": "C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.",
+    "intro_head_p2": "C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.",
+    "intro_tail": "Government of India has awarded the 'National Energy Conservation Award 2018'. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.",
+    "mid_section_text": "INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.",
+    "image_caption": "Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors",
+    "footer_head": "C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS",
+    "footer_text": "CHEMICALS & PROCESS | POWER | WATER & WASTE WATER | OILS & GAS | PHARMA | SUGARS & DISTILLERIES | PAPER & PULP | MARINE & DEFENCE | METAL & MINING | FOOD & BEVERAGE | PETROCHEMICAL & REFINERIES |SOLAR | BUILDING | HVAC | FIRE FIGHTING | AGRICULTURE & RESIDENTIAL",
+    "contact": "Toll Free 1800 200 1234",
+    "facebook": "www.facebook.com/cripumps",
+    "website": "www.crigroups.com"
+}
+
+module.exports = data
